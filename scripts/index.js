0987bc228275aeb460c02eaf9daa1ab904b293e2
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2025-03-05 11:35:44 -0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
-const slides = document.querySelectorAll('.slide');
-if (slides.length > 0) {
-    let currentSlide = 0;
+const texts = [
+    "Vive la Pasión del Tango",
+    "Siente el Ritmo en tu Alma",
+    "Baila con el Corazón"
+];
+let currentIndex = 0;
 
-    function nextSlide() {
-        slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide + 1) % slides.length;
-        slides[currentSlide].classList.add('active');
-    }
-
-    setInterval(nextSlide, 5000);
+function changeText() {
+    const textElement = document.getElementById('slider-text');
+    textElement.textContent = texts[currentIndex];
+    currentIndex = (currentIndex + 1) % texts.length;
 }
+
+setInterval(changeText, 5000);
 
 document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll("section");
@@ -37,14 +39,27 @@ document.addEventListener("DOMContentLoaded", function () {
 const newsContainer = document.getElementById('news-container');
 if (newsContainer) {
     const newsItems = [
-        { title: "Nuevo Espectáculo: 'Tango Nocturno'", content: "No te pierdas nuestro último espectáculo que combina tango tradicional con elementos de danza contemporánea." },
-        { title: "Clases Online Disponibles", content: "Ahora puedes aprender tango desde la comodidad de tu hogar con nuestras nuevas clases en línea." },
-        { title: "Colección de Moda Otoño/Invierno", content: "Descubre nuestra nueva colección de ropa y accesorios inspirados en el tango argentino." }
+        { 
+            title: "Un poco de historia del tango", 
+            content: "El tango es uno de los géneros musicales más icónicos y emblemáticos de la cultura argentina, y su origen se remonta a la ciudad de Buenos Aires a fines del siglo XIX. ",
+            link: "https://argentina-tango.net/2023/03/05/un-poco-de-historia-del-tango/"
+        },
+        { 
+            title: "Clases Online Disponibles", 
+            content: "Ahora puedes aprender tango desde la comodidad de tu hogar con nuestras nuevas clases en línea.",
+            link: "https://hotmart.com/es/marketplace/productos/20-lecciones-de-tango/F62016758K"
+        },
+        { 
+            title: "Colección de Moda Otoño/Invierno", 
+            content: "Descubre nuestra nueva colección de ropa y accesorios inspirados en el tango argentino.",
+            link: "/pages/otonio-invierno-2025.pdf"
+        }
     ];
 
     newsItems.forEach(item => {
-        const newsItem = document.createElement('div');
+        const newsItem = document.createElement('a');
         newsItem.classList.add('news-item');
+        newsItem.href = item.link;
         newsItem.innerHTML = `
             <h3>${item.title}</h3>
             <p>${item.content}</p>
@@ -52,7 +67,6 @@ if (newsContainer) {
         newsContainer.appendChild(newsItem);
     });
 }
-
 function initMap() {
     const map = new google.maps.Map(document.getElementById('google-map'), {
         center: { lat: -25.600018652464488, lng: -54.57124648491867 },
